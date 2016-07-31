e42625dc0e38ea6322806d6eb4acab4b78acef15
Tom Gillard <tomgillar@gmail.com>
2016-07-31 13:54:50 +1000

    replaced console log in index with react router component to render our app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('welcome to our React application');
+import 'babel-polyfill'; // Transpiles non standard ES6 features: should use individual ones being used rather than this blanket polyfill
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+import './styles/styles.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+render(
+  <Router history={browserHistory} routes={routes} />,
+  document.getElementIdById('app')
+);
