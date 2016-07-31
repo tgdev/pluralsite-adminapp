e1c95a4942351a07aa06ea092aa55e550957c391
Tom Gillard <tomgillar@gmail.com>
2016-07-31 14:40:03 +1000

    adding new courses page component to app

---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,14 @@
+import React, { PropTypes } from 'react';
+
+class CoursesPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Courses</h1>
+        <p>Available Pluralsight courses.</p>
+      </div>
+    );
+  }
+}
+
+export default CoursesPage;
