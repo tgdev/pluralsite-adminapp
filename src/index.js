c59bc0e88686695d1b6dafb84878c24e41611fcd
Tom Gillard <tom.gillard@getstarted.net.au>
2016-08-01 13:58:35 +1000

    adding store to exising app

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import 'babel-polyfill'; // Transpiles non standard ES6 features: should use ind
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
 import routes from './routes';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './styles/styles.css';
 
+// We can pass in initialState from api call or localStorage here,
+// otherwise set initialState in reducers
+const store = configureStore();
+
 render(
-  <Router history={browserHistory} routes={routes} />,
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>,
   document.getElementById('app')
 );
