259771f534f4ce30a03bc96bc934af68a138a802
Tom Gillard <tom.gillard@getstarted.net.au>
2016-08-01 13:43:34 +1000

    adding course reducer

---
+++ src/reducers/courseReducer.js
@@ -0,0 +1,13 @@
+// Pure functions for Courses
+
+export default function courseReducer(state = [], action) {
+  switch (action.type) {
+    case 'CREATE_COURSE':
+      return [...state,
+        Object.assign({}, action.course)
+      ];
+
+    default:
+      return state;
+  }
+}
